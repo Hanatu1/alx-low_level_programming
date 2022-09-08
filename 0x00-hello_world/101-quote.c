e6bee1687e555602ebbca7a4468ed35cceada7a9
Hanatu1 <hanatuonozasi@gmail.com>
2022-09-08 16:41:31 -0700

    My low level programming commit #8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unstdi.h>
 
 /**
@@ -5,11 +6,11 @@
  * - Dora Korpar, 2015-10-19"
  *   followed by a new line, to the standard error.
  *
- * Return: Always 1.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
- * 
+
