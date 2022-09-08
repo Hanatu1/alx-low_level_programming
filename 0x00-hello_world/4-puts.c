c73acb335a63c77104242e7292b490c657d6bf1c
Hanatu1 <hanatuonozasi@gmail.com>
2022-09-08 08:37:52 -0700

    My alx lowlevel programming commit #4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
- *  *Description:main - prints out a string to stdout.
- *   * return:0 if success
- *    */
- int main(void)
+ * main - print the string in the put function
+ *
+ *  *Description: using the main function
+ *  this program prints "Programming is like building a multilingual puzzle
+ *  Return: 0
+ *  */
+int main(void)
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
 				return (0);
 }
