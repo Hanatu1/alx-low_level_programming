481e0045c95039ae965d1ea3559f44a98c126d7a
Hanatu1 <hanatuonozasi@gmail.com>
2022-09-21 13:03:13 -0700

    Task #0 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
- *
+ * 
  * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
@@ -27,4 +27,3 @@ char *_strcat(char *dest, char *src)
 	dest[i] = '\0';
 
 	return (dest);
-}
