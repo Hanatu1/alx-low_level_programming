bd65543e32d3e26264589f2cf9d20a01388fa8cf
Hanatu Momohjimoh <106560426+Hanatu1@users.noreply.github.com>
2022-10-01 03:25:42 +0100

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code for Holberton School students.
- *
- * @c: is an integer param
- *
- * Return: Always 0.
- */
-
+*_isdigit - a function that checks for digit
+*@c: a character
+*Return: 1 if int c is a digit, 0 otherwise
+*/
 
 int _isdigit(int c)
 {
-
-	return (c >= 48 && c <= 57);
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
