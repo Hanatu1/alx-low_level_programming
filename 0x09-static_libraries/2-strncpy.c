6913e46b15f3b8d7baf6f72a43d54c6deed9537c
Hanatu Momohjimoh <106560426+Hanatu1@users.noreply.github.com>
2022-10-01 03:40:27 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * _strncpy - two words
- * @dest : pointer to char params
- * @src : pointer to char params
- * @n : int params
- * Return: *dest
+ * _strncpy - copies a string up to n
+ * @dest: Destination of the pointer to the string
+ * @src: Source of the pointer to the string to copy
+ * @n: Number of bytes to copy.
+ * Return: Pointer to the destination string.
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -13,13 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	for ( ; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+		dest[i]	= src[i];
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
