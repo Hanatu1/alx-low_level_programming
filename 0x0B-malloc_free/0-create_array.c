e459037b85e11ac13edb65ae81dfbef34c9ef323
Hanatu1 <hanatuonozasi@gmail.com>
2022-10-04 01:18:19 -0700

    My memory allocation task # 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,18 +9,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *cr;
+	char *array;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
+	array = malloc(sizeof(c) * size);
 
-	cr = malloc(sizeof(c) * size);
-
-	if (cr == NULL)
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		cr[i] = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-	return (cr);
+	return (array);
+}
